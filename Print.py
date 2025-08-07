791f38730d5c48e9e01773729ba07be9b7c98bc0
arthurflorentino <arthurflorentino.4@gmail.com>
2025-08-07 17:26:01 -0300

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,17 @@
+print('1) Olá, Arthur!\n' 'Tudo bem?\n')
+
+print('2) Me chamo Arthur!', end=' ')
+print('Como vai?\n')
+
+print('3) Meu nome é ', end="")
+print('Arthur!\n')
+
+print("4) Meu", "nome", "é", "Arthur", "Silva", "Florentino.", sep="-")
+
+print('\n5) Olá!\n')
+
+print('6) Meu', 'nome', 'é', sep='_', end='*')
+print('Arthur', 'Silva', sep='*', end='*\n')
+
+print("\n7) Arthur", "Florentino", sep="&",)
+
