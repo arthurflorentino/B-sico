b22534f4e77bc91b73a04080b68168d635bc8ac7
arthurflorentino <arthurflorentino.4@gmail.com>
2025-08-08 17:19:06 -0300

    Add files via upload

---
+++ Print_2.py
@@ -0,0 +1,29 @@
+# Número Octal
+print('1)', 0o123)
+
+# Número Hexadecimal
+print(f'2) {0x123}')
+
+# Notação Científica
+print('3)',3E8)
+
+# Valor Literal Retorna Notação Científica
+print('4)',0.0000000000000000000001)
+
+# Aspas dentro de aspas sem gerar erro 1
+print("5) Eu gosto de \"Python\"")
+
+# Aspas dentro de aspas sem gerar erro 2
+print('6) Eu gosto de "Python"')
+
+# Apóstrofo em uma string colocada entre apóstrofos
+print('7) I\'m Arthur.')
+
+# 1 > 0?
+print('8)',True > False)
+
+# Desafio 1
+print('9) "Eu sou" \n""aprendizado"" \n"""Python"""')
+
+# Desafio 2
+print("10) \"Eu sou\"\n\"\"aprendizado\"\"\n\"\"\"Python\"\"\"")
